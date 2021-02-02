f8846b3130a9e399c985b020ce39cda54947516b
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-02-02 14:00:47 -0800

    added more workout choices

--- seeders/20210202164118-demo-workout.js
+++ seeders/20210202164118-demo-workout.js
@@ -3,10 +3,28 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
       await queryInterface.bulkInsert('Workouts', [{
-        name: 'Yoga For Beginners in 20 Minutes',
+        name: 'Yoga in 20 Minutes',
         link: "https://www.youtube.com/watch?v=v7AYKMP6rOE",
-        description: "Yoga with Adrianne"
-      }], {});
+        description: "Beginnners Yoga with Adrianne"
+      },
+      {
+        name: 'Sullivan Canyon Moutain Biking',
+        link: "https://www.mtbproject.com/photo/7007130",
+        description: "Intermediate Single Track near Santa Monica, CA "
+      },
+      {
+        name: 'Portuguese Bend Reserve, Palos Verdes, CA',
+        link: "https://www.mtbproject.com/photo/7028505",
+        description: "Intermidiate fire road and singletrack with ocean views"
+      },
+      {
+        name: 'Cow Trail, Whittier, CA',
+        link:"https://www.mtbproject.com/photo/7034947",
+        description:"Intermediate Local Spot, tree-covered singletrack; darting, fast, smooth, freeway-like stretch of pure speed",
+      }
+      
+    
+    ], {});
 
   },
 
