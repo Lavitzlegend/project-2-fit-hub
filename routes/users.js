0288b0380d9b56c80c88882ce778c4db461d34b4
jordan-dyar <jordan.dyar218@gmail.com>
2021-02-02 10:24:56 -0600

    Verified index, signup, and login pages working

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,21 @@ const ctrl = require("../controllers");
 router.get("/", ctrl.users.homePage);
 
 //New route
-router.get("/signup", ctrl.users.signUp);
-router.post("/signup", ctrl.users.postTrainer);
+router.get("/signup", ctrl.users.renderSignup);
+router.post("/signup", ctrl.users.signup);
 
 //Show profile route
-router.get("/profile/:index", ctrl.users.show);
+router.get("/profile/:index", ctrl.users.renderProfile);
 
 //Login route
-router.get("/login", ctrl.users.login);
-router.post("/login", ctrl.users.loginTrainer);
+router.get("/login", ctrl.users.renderLogin);
+router.post("/login", ctrl.users.login);
 
 //Edit route
-router.put("/profile/:index", ctrl.users.putTrainer);
+router.put("/profile/:index", ctrl.users.addWorkout);
+// router.put("/profile/edit/:index", ctrl.users.editUser);
 
 //Delete route
-router.delete("/profile/:index", ctrl.users.deleteTrainer);
+// router.delete("/profile/edit/:index", ctrl.users.deleteUser);
 
 module.exports = router;
