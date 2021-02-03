e36b7b37504af45c3e577c35f2de7f3d7f4a0485
jordan-dyar <jordan.dyar218@gmail.com>
2021-02-03 15:07:06 -0600

    Changed seeder links to use /embed instead of /watch

--- seeders/20210202164118-demo-workout.js
+++ seeders/20210202164118-demo-workout.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
       await queryInterface.bulkInsert('Workouts', [{
         name: 'Yoga For Beginners in 20 Minutes',
-        link: "https://www.youtube.com/watch?v=v7AYKMP6rOE",
+        link: "https://www.youtube.com/embed?v=v7AYKMP6rOE",
         description: "Yoga with Adrianne"
       }], {});
 
