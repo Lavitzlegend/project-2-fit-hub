ab73185a8320061a676876ca33423262acc09dd4
jordan-dyar <jordan.dyar218@gmail.com>
2021-02-03 15:51:46 -0600

    finished CrossFit embed URLs

--- seeders/20210202164118-demo-workout.js
+++ seeders/20210202164118-demo-workout.js
@@ -3,9 +3,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
       await queryInterface.bulkInsert('Workouts', [{
-        name: 'Yoga For Beginners in 20 Minutes',
-        link: "https://www.youtube.com/embed?v=v7AYKMP6rOE",
-        description: "Yoga with Adrianne"
       }], {});
 
   },
